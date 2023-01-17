5e15e707e3b3deeb57949e20887774cb48a22eac
carlos-xbm <carlos.xbm@outlook.com>
2023-01-16 22:09:44 -0300

    Rotas iniciadas

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import theme from "assets/styles/theme";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import Router from "./router";
+
+const queryClient = new QueryClient();
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
