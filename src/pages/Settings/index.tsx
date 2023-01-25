d8f0af1d996b152ac6e71eef76bc3ea1bfa68f09
carlos-xbm <carlos.xbm@outlook.com>
2023-01-24 23:47:10 -0300

    iniciado

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,32 @@
+import { Outlet } from "react-router-dom";
+import Menu from "../../components/Menu";
+import NavColumn from "../../components/NavColumn";
+import { navigationItems } from "../../data/navigation";
+import { RoutePath } from "../../types/routes";
+import * as S from "./style";
+
+const Settings = () => {
+  return (
+    <S.Settings>
+      <Menu active={RoutePath.SETTINGS} navItems={navigationItems} />
+      <S.SettingsPage>
+        <header>
+          <S.SettingsPageHeaderTitle>Configurações</S.SettingsPageHeaderTitle>
+        </header>
+        <S.SettingsContent>
+          <S.SettingsContentSidebar>
+            <NavColumn activeRoute={RoutePath.SETTINGS_TABLES} />
+          </S.SettingsContentSidebar>
+          <S.SettingsContentBox>
+            <S.SettingsContentBoxEmpty>
+              Selecione uma categoria
+            </S.SettingsContentBoxEmpty>
+            <Outlet />
+          </S.SettingsContentBox>
+        </S.SettingsContent>
+      </S.SettingsPage>
+    </S.Settings>
+  );
+};
+
+export default Settings;
